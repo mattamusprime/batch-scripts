e68415fd82d9865950054b6f6efbe1459a666f78
Matthew Urch <urch.matthew@gmail.com>
2019-11-13 13:02:14 -0500

    Robocopy Scripts
    Adds robocopy scripts

---
+++ filesystem/rcfile.bat
@@ -0,0 +1,23 @@
+@echo off
+
+IF [%1] == [] GOTO PrintUsage
+IF [%2] == [] GOTO PrintUsage
+
+set currentfolder=%cd%
+set destinationfolder=%~2
+set filetocopy=%~1
+
+IF %destinationfolder:~-1%==\ SET destinationfolder=%destinationfolder:~0,-1%
+IF %filetocopy:~-1%==\ SET filetocopy=%filetocopy:~0,-1%
+
+@echo on
+robocopy "%currentfolder%" "%destinationfolder%" "%filetocopy%"
+
+GOTO Done
+
+:PrintUsage
+echo "File to Copy: filename of a file in the current directory you want copied"
+echo "Destination: where to copy the file"
+echo "Usage: rcfile filetocopy C:\example"
+
+:Done
---
+++ filesystem/rmir.bat
@@ -0,0 +1,3 @@
+call mkdirs "%~2"
+
+robocopy /Z /MIR "%~1" "%~2" 
