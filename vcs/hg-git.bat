7f03246674c9d6a74ebfabb4c0dfc8b4e8b0e98f
Matthew Urch <matthew.urch@transunion.com>
2019-11-27 10:38:36 -0500

    Update hg-git.bat
    adds support to hg-git for relative paths

--- vcs/hg-git.bat
+++ vcs/hg-git.bat
@@ -3,13 +3,37 @@
 IF [%1] == [] GOTO PrintUsage
 IF [%2] == [] GOTO PrintUsage
 
+echo Making dir for git repo...
 mkdir "%~2"
-cd "%~2"
+
+echo Saving paths
+set START_ABS_PATH=%CD%
+set HG_ABS_PATH=
+set GIT_ABS_PATH=
+
+pushd "%~1"
+set HG_ABS_PATH=%CD%
+popd
+pushd "%~2"
+set GIT_ABS_PATH=%CD%
+popd
+
+echo HG path - %HG_ABS_PATH%
+echo GIT path - %GIT_ABS_PATH%
+
+echo Making git repo...
+mkdir %GIT_ABS_PATH%
+cd %GIT_ABS_PATH%
 git init --bare .git
-cd "%~1"
+
+echo Preparing hg repo
+cd %HG_ABS_PATH%
 hg bookmark -r default master
-hg push "%~2"
-cd "%~2"
+
+echo Pushing hg repo to git repo
+hg push %GIT_ABS_PATH%
+cd %GIT_ABS_PATH%
+
 git config --bool core.bare false
 git checkout master
 
@@ -19,3 +43,4 @@ GOTO Done
 echo "usage: hg-git [hg-repo-path] [git-repo-path]"
 
 :Done
+cd %START_ABS_PATH%
